93448b91681c5114cb2ed8c96f955694f104007b
xuxm <xuxmhao@qq.com>
2026-01-28 22:36:13 +0800

    改回user-agent

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const glados = async () => {
     const headers = {
       'cookie': cookie,
       'referer': 'https://glados.cloud',
-      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36',
+      'user-agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
     }
     const checkin = await fetch('https://glados.cloud/api/user/checkin', {
       method: 'POST',
